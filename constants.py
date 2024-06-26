5662728232a4c704fde85bb2bacd095a61ac72b7
Sergei Kravchuk <advisor5@yandex.ru>
2024-06-26 10:10:20 +0300

    edit constants.py: add class Url, HOST, NAME; del URL_REG

--- constants.py
+++ constants.py
@@ -1,6 +1,8 @@
 class Constants:
     EMAIL = 'sergei_kravchuk10111@yyy.ru'
     PASSWORD = 'space12'
-    URL_SITE = 'https://stellarburgers.nomoreparties.site/'
-    URL_REG = 'https://stellarburgers.nomoreparties.site/register'
-    URL_LOGIN = 'https://stellarburgers.nomoreparties.site/login'
+    NAME = "Sergei"
+
+class Url:
+    HOST = 'https://stellarburgers.nomoreparties.site/'
+    URL_LOGIN = f'{HOST}login'
