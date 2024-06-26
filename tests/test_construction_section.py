1be2f4f5ecfb5ce2ea8f77803b842016e131b6c2
Sergei Kravchuk <advisor5@yandex.ru>
2024-06-26 10:18:15 +0300

    edit test_construction_section.py: edit assert, edit style PEP8, edit cognates

--- tests/test_construction_section.py
+++ tests/test_construction_section.py
@@ -1,43 +1,20 @@
+import pytest
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
+from selenium.webdriver.common.by import By
 from locators import Locators
-from constants import Constants
 
-# Проверка, перехода к разделу "Начинки":
+
 class TestMesto:
-    def test_transfer_to_account_by_constructor(self, driver: WebDriver):
-        assert driver.current_url == Constants.URL_SITE  
+    @pytest.mark.parametrize('name_section', ['Булки', 'Начинки', 'Соусы'])
+    def test_transfer_between_sections(self, name_section, login: WebDriver):
+        driver = login
         
-        # Поиск и клик по кнопке "Личный кабинет"
-        driver.find_element(*Locators.MAIN_PERSONAL_AC).click()
-        # Ожидание загрузки страницы авторизации
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE))
-
-        # Заполнить поля: "Email", "Пароль", нажать кнопку "Войти"
-        driver.find_element(*Locators.IN_FIELD_EMAIL).send_keys(Constants.EMAIL)
-        driver.find_element(*Locators.IN_FIELD_PASSWORD).send_keys(Constants.PASSWORD)
-        driver.find_element(*Locators.IN_BUTTON).click()
-
-        # Ожидание авторизации и загрузки страницы конструктора заказов 
-        title = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_BURGERS)).text
-        assert title == "Соберите бургер"
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_BURGERS))
+        element = driver.find_element(By.XPATH, f".//span[text()='{name_section}']")
+        driver.execute_script("arguments[0].click();", element)
         
-        driver.find_element(*Locators.MAIN_TAB_FILLING).click()
-        title_filling = driver.find_element(*Locators.MAIN_TITLE_FILLING)
-        driver.execute_script("arguments[0].scrollIntoView();", title_filling)
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_TITLE_FILLING))
-        assert title_filling.text == 'Начинки' 
-
-        driver.find_element(*Locators.MAIN_TAB_SOUS).click()
-        title_sous = driver.find_element(*Locators.MAIN_TITLE_SOUS)
-        driver.execute_script("arguments[0].scrollIntoView();", title_sous)
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_TITLE_SOUS))
-        assert title_sous.text == 'Соусы' 
-
-        driver.find_element(*Locators.MAIN_TAB_BUNS).click()
-        title_buns = driver.find_element(*Locators.MAIN_TITLE_BUNS)
-        driver.execute_script("arguments[0].scrollIntoView();",title_buns)
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_TAB_BUNS))
-        assert title_buns.text == 'Булки' 
+        activated_class = driver.find_element(*Locators.MAIN_CURRENT_TAB)
+        parent_element = driver.find_element(By.XPATH, f".//span[text()='{name_section}']/parent::div")
+        assert parent_element == activated_class
