7903fa4c7c555c98aeab2ae8463260aa0f2f3a01
Sergei Kravchuk <advisor5@yandex.ru>
2024-06-26 10:06:44 +0300

    edit conftest.py: edit style PEP8, del assert, add fixture gen_email

--- conftest.py
+++ conftest.py
@@ -1,38 +1,31 @@
 import pytest
 from selenium import webdriver
-from constants import Constants
-from locators import Locators
-
-# from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from constants import Constants
+from locators import Locators
+from constants import Url
+import random
 
 
- 
-@pytest.fixture #(scope="session") УБРАТЬ
+@pytest.fixture
 def driver():
     browser = webdriver.Chrome()
-    browser.get(Constants.URL_SITE)
+    browser.get(Url.HOST)
     yield browser
-
     browser.quit()
 
 @pytest.fixture
 def login(driver):
-        assert driver.current_url == Constants.URL_SITE  
-        # Поиск и клик по кнопке "Личный кабинет"
-        driver.find_element(*Locators.MAIN_PERSONAL_AC).click()
-        # Явное ожидание загрузки страницы авторизации
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE))
+    driver.find_element(*Locators.MAIN_PERSONAL_AC).click()
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE))
 
-        # Заполнить поля: "Email", "Пароль", нажать кнопку "Войти"
-        driver.find_element(*Locators.IN_FIELD_EMAIL).send_keys(Constants.EMAIL)
-        driver.find_element(*Locators.IN_FIELD_PASSWORD).send_keys(Constants.PASSWORD)
-        driver.find_element(*Locators.IN_BUTTON).click()
+    driver.find_element(*Locators.IN_FIELD_EMAIL).send_keys(Constants.EMAIL)
+    driver.find_element(*Locators.IN_FIELD_PASSWORD).send_keys(Constants.PASSWORD)
+    driver.find_element(*Locators.IN_BUTTON).click()
+    return driver
 
-        #Найти кнопку Личный кабинет и проверить логин  
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_PERSONAL_AC)).click()
-        login = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PERSONAL_FIELD_LOGIN)).get_attribute('value')
-        assert login == Constants.EMAIL
-
-        return driver
+@pytest.fixture
+def gen_email():
+    email = f"sergei_kravchuk10{random.randint(100, 999)}@ya.ru"
+    return email
