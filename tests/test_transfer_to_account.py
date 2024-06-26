a186befd1efbe04b6f67f5865e2365c916d9a4aa
Sergei Kravchuk <advisor5@yandex.ru>
2024-06-26 10:15:45 +0300

    edit test_transfer_to_account.py: edit assert, edit style PEP8, edit cognates

--- tests/test_transfer_to_account.py
+++ tests/test_transfer_to_account.py
@@ -1,33 +1,15 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 from locators import Locators
 from constants import Constants
 
-# Проверка перехода по клику на «Личный кабинет»
 
 class TestMesto:
-    def test_transfer_to_accaunt(self, driver: WebDriver):
-        assert driver.current_url == Constants.URL_SITE  
-        
-        # Поиск и клик по кнопке "Личный кабинет"
-        driver.find_element(*Locators.MAIN_PERSONAL_AC).click()
-        # Ожидание загрузки страницы авторизации
-        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE))
 
-        # Заполнить поля: "Email", "Пароль", нажать кнопку "Войти"
-        driver.find_element(*Locators.IN_FIELD_EMAIL).send_keys(Constants.EMAIL)
-        driver.find_element(*Locators.IN_FIELD_PASSWORD).send_keys(Constants.PASSWORD)
-        driver.find_element(*Locators.IN_BUTTON).click()
+    def test_transfer_to_accaunt(self, login: WebDriver):
+        driver = login
 
-        #Найти кнопку Личный кабинет и проверить логин  
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_PERSONAL_AC)).click()
-        login = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PERSONAL_FIELD_LOGIN)).get_attribute('value')
-        assert login == Constants.EMAIL
-
-        #Найти кнопку Личный кабинет и войти 
-        # WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PERSONAL_TITLE))
-        # assert driver.find_element(*Locators.PERSONAL_FIELD_LOGIN).get_attribute('value') == Constants.EMAIL
-
-        
+        email = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PERSONAL_FIELD_LOGIN))
+        assert email.get_attribute("value") == Constants.EMAIL
