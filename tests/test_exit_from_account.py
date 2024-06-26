2fb4e04c502562dd40e25b9c52767dd73991f713
Sergei Kravchuk <advisor5@yandex.ru>
2024-06-26 10:13:57 +0300

    edit test_exit_from_account.py: edit assert, edit style PEP8, edit cognates

--- tests/test_exit_from_account.py
+++ tests/test_exit_from_account.py
@@ -1,17 +1,16 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 from locators import Locators
-
-# Проверка выхода по кнопке «Выйти» в личном кабинете
+from constants import Url
 
 class TestMesto:
+
+
     def test_exit_from_account(self, login: WebDriver):
         driver = login
 
-        # Поиск и клик по кнопке «Выйти» в личном кабинете
-        driver.find_element(*Locators.PERSONAL_EXIT).click()
-        input = WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE)).text
-        assert input == 'Вход'
-        
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.MAIN_PERSONAL_AC)).click()
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.PERSONAL_EXIT)).click()
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located(Locators.IN_PAGE))
+        assert driver.current_url == Url.URL_LOGIN
